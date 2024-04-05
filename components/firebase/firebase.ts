9b8c15b27f4b8da56b943565165879534a19fc8e
daveclinton <clintondavid46@gmail.com>
2024-04-05 07:31:38 +0300

    fix(chore): Update env variable

--- components/firebase/firebase.ts
+++ components/firebase/firebase.ts
@@ -5,7 +5,7 @@ import { isDev } from '../isDev';
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROHECT_ID,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
